fa225e67c0df7656591296f528156ebb6e3b4b0a
helloman <zcj23085@gmail.com>
2021-08-09 20:20:45 +0800

    添加有效字符串

--- src/main/scala/leetcode/dp/MaxSubArray.scala
+++ src/main/scala/leetcode/dp/MaxSubArray.scala
@@ -6,11 +6,13 @@ package leetcode.dp
 object MaxSubArray {
 
   def apply(nums:Array[Int]):Int = {
-    var currMax =0
+    if (nums.length==1)return nums(0)
     var ans = 0
-    for (i<-nums.indices){
-      currMax = Math.max(currMax+nums(i),nums(i))
-      ans = Math.max(ans,currMax)
+    val dp = new Array[Int](nums.length)
+    dp(0)= nums(0)
+    for (i<-1 until nums.length){
+      dp(i)= Math.max(dp(i-1)+ nums(i),nums(i))
+      ans = Math.max(ans,dp(i))
     }
     ans
   }
---
+++ src/test/scala/leetcode/dp/MaxSubArrayTest.scala
@@ -0,0 +1,15 @@
+package leetcode.dp
+
+import org.scalatest.FunSuite
+import org.junit.Assert
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+@RunWith(classOf[JUnitRunner])
+class MaxSubArrayTest extends FunSuite{
+
+  test("MaxSubArray"){
+    println(MaxSubArray(Array(-2,1,-3,4,-1,2,1,-5,4)))
+    println(MaxSubArray(Array(1)))
+  }
+}
