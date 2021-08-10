6a5b41ecfe335e34babb6213ef1d6b9a6f0f6711
helloman <zcj23085@gmail.com>
2021-08-10 11:31:15 +0800

    添加有效字符串

--- src/main/scala/leetcode/dp/HouseRobTwo.scala
+++ src/main/scala/leetcode/dp/HouseRobTwo.scala
@@ -1,34 +1,28 @@
 package leetcode.dp
 
+import java.util
+
 object HouseRobTwo {
 
   def apply(nums: Array[Int]): Int = {
 
-    if (nums.length<=2){
-      val res = nums.length match {
-        case 0 => 0
-        case 1 => nums(0)
-        case 2=> Math.max(nums(0),nums(1))
-      }
-      return res
-    }
+    if (nums.length==0 ) return 0
+    if (nums.length==1) return nums(0)
+    if (nums.length==2) return Math.max(nums(0),nums(1))
 
-
-    Math.max(myRob(nums,0,nums.length-2),myRob(nums,1,nums.length-1))
-
+    val rob1 = myRob(util.Arrays.copyOfRange(nums,0,nums.length-1))
+    val rob2 = myRob(util.Arrays.copyOfRange(nums,1,nums.length))
+    Math.max(rob1,rob2)
   }
 
-  def myRob(nums:Array[Int],start:Int, end:Int):Int ={
-    var res = 0
-    val dp = new Array[Int](end-start)
-    dp(0) =  nums(start)
-    dp(1) = Math.max(nums(start),nums(start+1))
-    for (i<-start+2 until end) {
+  def myRob(nums:Array[Int]):Int ={
+    val dp = new Array[Int](nums.length-1)
+    dp(0) =  nums(0)
+    dp(1) = Math.max(nums(0),nums(1))
+    for (i<-2 until  nums.length) {
       dp(i) = Math.max(dp(i - 2) + nums(i), dp(i - 1))
-      res = Math.max(res, dp(i))
     }
-
-    res
+    dp(nums.length-1)
   }
 
 }
